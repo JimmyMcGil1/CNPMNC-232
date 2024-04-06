75cbd50becdcb12384b5cb1aa9b2c2c35f00020d
Huy Tran Dinh <trandinhhuy1996tv@gmail.com>
2024-04-06 10:02:15 +0700

    init fe

---
+++ cnpmnc_232_frontend/src/main/java/cnpmnc_232/cnpmnc_232_backend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cnpmnc_232.cnpmnc_232_backend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Cnpmnc232BackendApplication.class);
+    }
+
+}
