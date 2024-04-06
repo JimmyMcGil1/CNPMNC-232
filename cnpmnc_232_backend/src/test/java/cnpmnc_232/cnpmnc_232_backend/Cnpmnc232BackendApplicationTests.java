0a02b2ededb164cb122ec913ca6abed43cfaea81
JimmyMcGil1 <92731328+JimmyMcGil1@users.noreply.github.com>
2024-04-06 09:17:48 +0700

    build: init backend

---
+++ cnpmnc_232_backend/src/test/java/cnpmnc_232/cnpmnc_232_backend/Cnpmnc232BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cnpmnc_232.cnpmnc_232_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cnpmnc232BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
