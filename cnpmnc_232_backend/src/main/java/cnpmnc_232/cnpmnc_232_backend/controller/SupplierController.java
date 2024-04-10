cebc72f672e9de0181f0ecf337339068b9a748ab
JimmyMcGil1 <92731328+JimmyMcGil1@users.noreply.github.com>
2024-04-10 20:58:31 +0700

    build: add suppliers/all api

--- cnpmnc_232_backend/src/main/java/cnpmnc_232/cnpmnc_232_backend/controller/SupplierController.java
+++ cnpmnc_232_backend/src/main/java/cnpmnc_232/cnpmnc_232_backend/controller/SupplierController.java
@@ -1,6 +1,8 @@
 package cnpmnc_232.cnpmnc_232_backend.controller;
 
 import cnpmnc_232.cnpmnc_232_backend.dto.request.SupplierDto;
+import cnpmnc_232.cnpmnc_232_backend.dto.response.SupplierRespDto;
+import cnpmnc_232.cnpmnc_232_backend.entity.Order;
 import cnpmnc_232.cnpmnc_232_backend.entity.Supplier;
 import cnpmnc_232.cnpmnc_232_backend.repository.ItemRepository;
 import cnpmnc_232.cnpmnc_232_backend.repository.OrderRepository;
@@ -8,16 +10,13 @@ import cnpmnc_232.cnpmnc_232_backend.repository.SupplierRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @Getter
 @Setter
@@ -39,9 +38,19 @@ public class SupplierController {
 
     @GetMapping("/all")
     public ResponseEntity<?> getAllSuppliers() {
-        List<Supplier> rtnList = new ArrayList<>();
-        rtnList = StreamSupport.stream(supplierRepo.findAll().spliterator(), false)
-                .collect(Collectors.toList());
-        return new ResponseEntity<>(rtnList, HttpStatus.OK);
+        try {
+            List<Supplier> getSupps = supplierRepo.findAll();
+            List<SupplierRespDto> suppsDto = getSupps.stream()
+                    .map(supp -> {
+                        List<Integer> ordersId = supp.getOrders().stream().map(Order::getId).toList();
+                        return new SupplierRespDto(supp.getId(), supp.getName(), supp.getAddress(), supp.getPhone(),
+                                supp.getEmail(), ordersId);
+                    })
+                    .toList();
+
+            return new ResponseEntity<>(suppsDto, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Fail to get all supplier:" + e.getMessage(), HttpStatus.OK);
+        }
     }
 }
--- cnpmnc_232_backend/src/main/java/cnpmnc_232/cnpmnc_232_backend/entity/Supplier.java
+++ cnpmnc_232_backend/src/main/java/cnpmnc_232/cnpmnc_232_backend/entity/Supplier.java
@@ -1,16 +1,10 @@
 package cnpmnc_232.cnpmnc_232_backend.entity;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import org.hibernate.annotations.CollectionId;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-import org.springframework.cglib.core.Local;
 
 import java.util.List;
 
@@ -33,7 +27,7 @@ public class Supplier {
     @Column(name = "EMAIL")
     private String email;
     @OneToMany(mappedBy = "supplier", cascade = CascadeType.ALL)
-    private List<Order> ordersId;
+    private List<Order> orders;
 
     public Supplier(String name, String address, String phone, String email) {
         this.name = name;
